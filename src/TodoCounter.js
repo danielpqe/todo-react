28d3670ecd1357cfbbe64617012b31b7ca16afe7
Daniel Quispe <v-daquis@microsoft.com>
2021-09-02 22:48:54 -0500

    initial commit

---
+++ src/TodoCounter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './TodoCounter.css';
+
+function TodoCounter (){
+    return (
+        <h1 className="TodoCounter">Has completado 2 de 3 TODOs</h1>
+    ) 
+    
+}
+
+export default TodoCounter;
